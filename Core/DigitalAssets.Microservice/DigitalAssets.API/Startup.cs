f343cda09f7b25e651e916c991fa98af8c348efc
Sundar Urs <sundarkurs@gmail.com>
2021-06-27 16:41:05 +0530

    added a swagger plugin

--- Core/DigitalAssets.Microservice/DigitalAssets.API/Startup.cs
+++ Core/DigitalAssets.Microservice/DigitalAssets.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,18 @@ namespace DigitalAssets.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Digital Assets Microservice", Version = "v1" });
+            });
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -41,6 +54,14 @@ namespace DigitalAssets.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reactivities Service");
+            });
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
